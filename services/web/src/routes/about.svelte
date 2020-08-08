3bfbab4a3c3c025c41a56790b6fa570c50bdcbc2
Andrii Ieroshevych <andrii.iero@gmail.com>
2020-08-08 19:20:16 +0300

    sert

--- services/web/src/routes/about.svelte
+++ services/web/src/routes/about.svelte
@@ -1,14 +1,7 @@
 <script>
   import FeatureToggle from "../components/feature-toggle.svelte";
   import { FEATURE_TOGGLES } from "../feature-toggles";
-<<<<<<< HEAD
-  import Card from "../components/card.svelte"
 
-
-	const imageSrc = 'https://habrastorage.org/getpro/habr/upload_files/a87/6a8/e95/a876a8e9579264bfc60373d04f3ff3c9.jpg'
-
-
-=======
   import ContactCard from "../components/contact-card.svelte";
 
  
@@ -21,7 +14,7 @@
     name: "Erin Lindford"
   }
   
->>>>>>> 11ef59e3852a5d9deb5b8c7a71f24a73f87a059f
+
 </script>
 
 <svelte:head>
