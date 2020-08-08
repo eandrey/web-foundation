11ef59e3852a5d9deb5b8c7a71f24a73f87a059f
Andriy Yeroshevych <eandrey.com@gmila.com>
2020-08-08 18:26:35 +0300

    blog post form

---
+++ services/web/src/components/new-blog-post.svelte
@@ -0,0 +1,29 @@
+<script>
+  import Button from "./ui-elements/button.svelte";
+  import InputGroup from "./ui-elements/input-group.svelte";
+
+  let title;
+  let content;
+
+  $: isPublishedDisabled = !(title && content);
+</script>
+
+<h1>Add new blog post</h1>
+<div class="mt-6">
+  <form>
+    <InputGroup
+      elementType="input"
+      label="Title"
+      id="title"
+      placeholder="Untitled"
+      bind:value={title} />
+    <InputGroup
+      elementType="textarea"
+      label="Content"
+      id="content"
+      placeholder="Your content ..."
+      bind:value={content} />
+      <Button disabled={isPublishedDisabled}>Publish</Button>
+
+  </form>
+</div>
--- services/web/src/routes/admin/index.svelte
+++ services/web/src/routes/admin/index.svelte
@@ -2,6 +2,7 @@
     import {goto} from '@sapper/app'
     import {onMount} from 'svelte'
     import {currentUser} from '../../stores/user.js'
+    import NewBlogPost from '../../components/new-blog-post.svelte'
 
     onMount(()=>{
         if(!currentUser){
@@ -11,6 +12,5 @@
 </script>
 
 {#if $currentUser}
-     <h1>Admin panel</h1>
-      <p>Only authenticated users have access to this page</p>
+     <NewBlogPost />
 {/if}
