7a1f528613fa02175d81242fbf0f9b76d716846b
Andriy Yeroshevych <eandrey.com@gmila.com>
2020-08-08 14:06:03 +0300

    Tailwind css added

--- services/web/src/routes/_layout.svelte
+++ services/web/src/routes/_layout.svelte
@@ -1,22 +1,24 @@
 <script>
-	import Nav from '../components/Nav.svelte';
-
-	export let segment;
+  import Nav from "../components/Nav.svelte";
+  import TailwindCSS from "../components/tailwindcss.svelte";
+  export let segment;
 </script>
 
 <style>
-	main {
-		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
+  main {
+    position: relative;
+    max-width: 56em;
+    background-color: white;
+    padding: 2em;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
 </style>
 
-<Nav {segment}/>
+<TailwindCSS />
+
+<Nav {segment} />
 
 <main>
-	<slot></slot>
+  <slot />
 </main>
