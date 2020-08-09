c13e0a38cc2ccd5ba94b3641c8d1c7a09214f2ca
Andriy Yeroshevych <eandrey.com@gmila.com>
2020-08-09 16:43:20 +0300

    read posts

--- services/web/src/components/new-blog-post.svelte
+++ services/web/src/components/new-blog-post.svelte
@@ -4,16 +4,27 @@
 
   let title;
   let content;
+
+  let successMessage;
+  let errorMessage;
+
   function publish() {
-      const slug = title.replace(/\s/g, "-").toLowerCase();
-      window.db.collection('posts').doc(slug).set({
-          title,
-          content,
-      }).then(() => {
-          console.log('Success')
-      }).catch((err) => {
-          console.log('Error', err)
+    const slug = title.replace(/\s/g, "-").toLowerCase();
+    window.db
+      .collection("posts")
+      .doc(slug)
+      .set({
+        title,
+        content
       })
+      .then(() => {
+        // console.log("Success");
+        successMessage = "Successfully published your post";
+      })
+      .catch(err => {
+        // console.log("Error", err);
+        errorMessage = "Oh no... there was an error publishing";
+      });
   }
   $: isPublishedDisabled = !(title && content);
 </script>
@@ -40,4 +51,9 @@
     <Button on:click={publish} disabled={isPublishedDisabled}>Publish</Button>
 
   </form>
+  {#if successMessage}
+    <p class="mt-2 text-sm">{successMessage}</p>
+  {:else if errorMessage}
+    <p class="mt-2 text-sm text-red-600">{errorMessage}</p>
+  {/if}
 </div>
--- services/web/src/routes/blog/index.svelte
+++ services/web/src/routes/blog/index.svelte
@@ -1,34 +1,53 @@
 <script context="module">
-	export function preload({ params, query }) {
-		return this.fetch(`blog.json`).then(r => r.json()).then(posts => {
-			return { posts };
-		});
-	}
+  export function preload({ params, query }) {
+    if (typeof window !== "undefined") {
+      return window.db
+        .collection("posts")
+        .get()
+        .then(querySnapshot => {
+          const posts = querySnapshot.docs.map(doc => ({
+            slug: doc.id,
+            ...doc.data()
+		  }));
+		  return {posts}
+        });
+    }
+    return null;
+  }
 </script>
 
 <script>
-	export let posts;
+  export let posts;
+  console.log(`posts
+	${posts}
+  `);
 </script>
 
 <style>
-	ul {
-		margin: 0 0 1em 0;
-		line-height: 1.5;
-	}
+  ul {
+    margin: 0 0 1em 0;
+    line-height: 1.5;
+  }
 </style>
 
 <svelte:head>
-	<title>Blog</title>
+  <title>Blog</title>
 </svelte:head>
 
 <h1>Recent posts</h1>
 
 <ul data-cy="blog-posts-list">
-	{#each posts as post}
-		<!-- we're using the non-standard `rel=prefetch` attribute to
+{#if posts}
+	 <!-- content here -->
+
+  {#each posts as post}
+    <!-- we're using the non-standard `rel=prefetch` attribute to
 				tell Sapper to load the data for the page as soon as
 				the user hovers over the link or taps it, instead of
 				waiting for the 'click' event -->
-		<li><a rel='prefetch' href='blog/{post.slug}'>{post.title}</a></li>
-	{/each}
+    <li>
+      <a rel="prefetch" href="blog/{post.slug}">{post.title}</a>
+    </li>
+  {/each}
+{/if}
 </ul>
